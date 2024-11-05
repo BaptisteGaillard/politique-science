09b390a1d19de2bb709bd6ee460b48e72157afc0
BaptisteGaillard <baptisteg.gaillard@gmail.com>
2024-11-05 14:54:58 +0000

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -6,7 +6,7 @@ const config = {
       long: "", // You can add a longer description if needed
     },
     FR: {
-      short: `Baptiste Gaillard est doctorant en sociologie depuis décembre 2021 au Laboratoire Interdisciplinaire Sciences Innovations Sociétés (UMR INRAE/LISIS)<a href="https://umr-lisis.fr/">ici</a>, et il est cofinancé par le projet de recherche ANR-19-MGPA-0010 WAYS-OUT.`,
+      short: `Baptiste Gaillard est doctorant en sociologie depuis décembre 2021 au Laboratoire Interdisciplinaire Sciences Innovations Sociétés (UMR INRAE/LISIS)<a href="https://umr-lisis.fr/">ici</a> et est cofinancé par le projet de recherche ANR-19-MGPA-0010 WAYS-OUT.`,
       long: "", // Idem, ajout d'une description longue si nécessaire
     },
   },
