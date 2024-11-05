090a12e6c3b8c828fa2be4b5651fa32e2ff1d520
BaptisteGaillard <baptisteg.gaillard@gmail.com>
2024-11-05 14:40:09 +0000

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,26 +1,24 @@
 const config = {
-  name: "Guillaume Levrier",
+  name: "Baptiste Gaillard",
   description: {
     EN: {
-      short: `Baptiste Gaillard is a PhD student in sociology since December 2021 at the Laboratoire Interdisciplinaire
-          Sciences Innovations Sociétés (UMR INRAE/LISIS)<a href="https://umr-lisis.fr/" a>. and his funded by the research project ANR-19-MGPA-0010 WAYS-OUT
-      `,
-      long: "",
+      short: `Baptiste Gaillard is a PhD student in sociology since December 2021 at the Laboratoire Interdisciplinaire Sciences Innovations Sociétés (UMR INRAE/LISIS)<a href="https://umr-lisis.fr/">here</a>, and he is co-funded by the research project ANR-19-MGPA-0010 WAYS-OUT.`,
+      long: "", // You can add a longer description if needed
     },
     FR: {
-      short: `
-      long: "",
+      short: `Baptiste Gaillard est doctorant en sociologie depuis décembre 2021 au Laboratoire Interdisciplinaire Sciences Innovations Sociétés (UMR INRAE/LISIS)<a href="https://umr-lisis.fr/">ici</a>, et il est cofinancé par le projet de recherche ANR-19-MGPA-0010 WAYS-OUT.`,
+      long: "", // Idem, ajout d'une description longue si nécessaire
     },
   },
   links: [
-    {
-      ,
-    },
     {
       label: "google scholar",
       value: "profile",
       url: "https://scholar.google.com/citations?user=jOxmy7IAAAAJ&hl=fr",
     },
   ],
-  email: { name: "baptisteg.gaillard", domain: "politique.science" },
+  email: {
+    name: "baptisteg.gaillard",
+    domain: "politique.science",
+  },
 };
